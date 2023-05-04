2e64d5e67ee6c769e61a4ffd939a8f3cef50a1d4
annabellrr <a.yusupova1@g.nsu.ru>
2023-05-04 23:04:56 +0700

    nm

---
+++ NM/NM.c
@@ -0,0 +1,274 @@
+#include "stdio.h"
+#include "malloc.h"
+#include "time.h"
+#include "stdlib.h"
+
+typedef struct matrixNxM
+{
+    float* data;
+    int n;
+    int m;
+}matrixNxM;
+
+//Инициализация матрицы NxM
+matrixNxM initMatrix(int n, int m)
+{
+    matrixNxM matrix;
+    matrix.n = n;
+    matrix.m = m;
+    matrix.data = malloc(sizeof(float) * n * m);
+    if (matrix.data == NULL)
+    {
+        matrix.n = 0;
+        matrix.m = 0;
+    }
+    return matrix;
+}
+
+//Заполнение матрицы случайными значениями
+void RandomMatrix(matrixNxM matrix)
+{
+    for (int i = 0; i < matrix.n * matrix.m; i++)
+        *(matrix.data + i) = rand();
+}
+
+//Matrix file input
+matrixNxM Read(FILE* fp)
+{
+    int n, m;
+    matrixNxM matrix;
+    fscanf_s(fp, "%d", &n);
+    fscanf_s(fp, "%d", &m);
+    matrix = initMatrix(n, m);
+    for (int i = 0; i < matrix.n; i++) {
+        for (int j = 0; j < matrix.m; j++)
+        {
+            fscanf_s(fp, "%f", matrix.data + i * matrix.n + j);
+        }
+    }
+    return matrix;
+}
+
+//Вывод матрицы
+void print(matrixNxM matrix)
+{
+    int i, j;
+    printf("Matrix:\n");
+    for (int i = 0; i < matrix.n; i++)
+    {
+        for (int j = 0; j < matrix.m; j++)
+            printf("\t %f", *(matrix.data + i * matrix.n + j));
+        printf("\n");
+    }
+}
+
+void PrintFile(matrixNxM matrix)
+{
+    int i, j;
+    FILE* fp;
+    fp = fopen_s("Result.txt", "w");
+
+    for (i = 0; i < matrix.n; ++i)
+    {
+        for (j = 0; j < matrix.m; j++)
+            fprintf(fp, "%f\t", *(matrix.data + i * matrix.n + j));
+        fprintf(fp, "\n");
+    }
+    fclose(fp);
+}
+
+//Сумма матриц Need free
+matrixNxM Sum(matrixNxM a, matrixNxM b)
+{
+    int i, j;
+    matrixNxM res;
+    res = initMatrix(a.n, a.m);
+    if ((a.n != b.n) || (a.m != b.m))
+    {
+        res.n = 0;
+        res.m = 0;
+
+        return res;
+    }
+
+    for (int i = 0; i < a.n; i++) {
+        for (int j = 0; j < a.m; j++)
+            *(res.data + i * res.n + j) = *(a.data + i * a.n + j) + *(b.data + i * b.n + j);
+    }
+    printf("Sum:\n");
+    return res;
+
+}
+
+//Произведение матриц Need free
+matrixNxM mult(matrixNxM a, matrixNxM b)
+{
+    matrixNxM res;
+    int i, k, j;
+    res = initMatrix(a.n, b.m);
+    if (a.m != b.n)
+    {
+        res.n = 0;
+        res.m = 0;
+        return res;
+
+    }
+
+    for (i = 0; i < a.n; i++)
+    {
+        for (j = 0; j < b.m; j++)
+        {
+            *(res.data + i * res.n + j) = 0;
+            for (k = 0; k < a.m; k++)
+                *(res.data + i * res.n + j) += *(a.data + i * a.n + k) * *(b.data + k * b.n + j);
+        }
+    }
+    printf("Multiplication:\n");
+    return res;
+}
+
+//Минор Need free()
+matrixNxM minor(matrixNxM matrix, int i0, int j0)
+{
+    int x, y;
+    x = 0;
+    y = 0;
+    matrixNxM minor;
+    minor = initMatrix(matrix.n - 1, matrix.m - 1);
+    for (int i = 0; i < matrix.n; i++)
+    {
+        if (i != i0)
+        {
+            for (int j = 0; j < matrix.m; j++)
+            {
+                if (j != j0)
+                {
+                    *(minor.data + x * minor.n + y) = *(matrix.data + i * matrix.n + j);
+                    y = y + 1;
+                }
+            }
+            x = x + 1;
+            y = 0;
+        }
+    }
+    return minor;
+}
+
+//Определитель
+float Det(matrixNxM matrix)
+{
+    float det = 0;
+    int sgn = 1;
+    matrixNxM mr;
+    if (matrix.n != matrix.m)
+        return 0;
+    if (matrix.n == 1)
+        return *(matrix.data);
+    for (int i = 0; i < matrix.n; i++)
+    {
+        mr = minor(matrix, 0, i);
+        det += *(matrix.data + 0 * matrix.n + i) * sgn * Det(mr);
+        sgn = sgn * (-1);
+    }
+
+    return det;
+}
+
+//Обратная Need free()
+matrixNxM inverse(matrixNxM matrix)
+{
+    matrixNxM res, mr;
+    res = initMatrix(matrix.n, matrix.m);
+    if (matrix.n != matrix.m)
+    {
+        res.m = 0;
+        res.n = 0;
+
+        return res;
+    }
+    float det;
+    det = Det(matrix);
+    if (det == 0)
+    {
+        res.n = 0;
+        res.m = 0;
+
+        return res;
+    }
+    for (int i = 0; i < matrix.n; i++) {
+        for (int j = 0; j < matrix.m; j++)
+        {
+            mr = minor(matrix, j, i);
+            if ((i + j) / 2 * 2 == (i + j))
+                *(res.data + i * res.n + j) = Det(mr) / det;
+            else
+                *(res.data + i * res.n + j) = (-1) * Det(mr) / det;
+        }
+        printf("Inverse:\n");
+        return res;
+    }
+}
+void Check(FILE* fp)
+{
+    if (fp == NULL)
+    {
+        printf("Error!");
+        return;
+    }
+}
+void main()
+{
+    int dir, n, m;
+    matrixNxM a, b, res;
+    printf("where to get the matrix, 1=File, 2=Random\n");
+    scanf_s("%d", &dir);
+    FILE* asrc = fopen_s("asrc.txt", "rt");
+    FILE* bsrc = fopen_s("bsrc.txt", "rt");
+    Check(asrc);
+    Check(bsrc);
+
+    switch (dir)
+    {
+    case 1:
+        a = Read(asrc);
+        b = Read(bsrc);
+        break;
+    case 2:
+        printf("Write n=row and m=column\n");
+
+        scanf_s("%d", &n);
+        scanf_s("%d", &m);
+
+        a = initMatrix(n, m);
+        printf("Write n=row and m=column\n");
+        scanf_s("%d", &n);
+        scanf_s("%d", &m);
+
+        b = initMatrix(n, m);
+        RandomMatrix(a);
+        RandomMatrix(b);
+        break;
+
+    default:
+        printf("ERROR");
+        return;
+    }
+
+    print(a);
+
+    print(b);
+
+    res = Sum(a, b);
+    print(res);
+    PrintFile(res);
+    free(res.data);
+    res = mult(a, b);
+    print(res);
+    free(res.data);
+    printf("Determinant a: %f\n", Det(a));
+    res = inverse(a);
+    print(res);
+    free(res.data);
+    free(a.data);
+    free(b.data);
+}
---
+++ dinText/dintext.c
@@ -0,0 +1,292 @@
+#include "stdio.h"
+#include "malloc.h"
+
+typedef struct text_type
+{
+	int size;
+	char* p;
+	int end;
+
+}text_type;
+
+text_type textReadStep(FILE* stream, text_type text, int buffer_s, char terminator)
+{
+	text_type newtext;
+	newtext.end = 0;
+	newtext.size = 0;
+	char* pbuffer;
+	int stopind = buffer_s - 1;
+	pbuffer = malloc(sizeof(char) * (buffer_s + 1));
+	if (pbuffer == NULL)
+	{
+		printf("textReadStep: No memory available!");
+		return text;
+	}
+
+	fgets(pbuffer, buffer_s + 1, stream);
+	for (int i = 0; i < buffer_s; i++)
+	{
+		if (*(pbuffer + i) == '\n')
+		{
+			stopind = i;
+			break;
+		}
+		if (*(pbuffer + i) == terminator)
+		{
+			stopind = i;
+			newtext.end = 1;
+			break;
+		}
+	}
+
+	newtext.size = text.size + stopind + 1;
+	newtext.p = malloc(sizeof(char) * newtext.size);
+	if (newtext.p == NULL)
+	{
+		printf("textReadStep: No memory available!");
+		return text;
+	}
+
+	for (int i = 0; i < text.size; i++)
+		*(newtext.p + i) = *(text.p + i);
+	if (text.size != 0)
+		free(text.p);
+	for (int i = text.size; i < newtext.size; i++)
+		*(newtext.p + i) = *(pbuffer + i - text.size);
+	free(pbuffer);
+	return newtext;
+}
+
+text_type getText(FILE* stream, char terminator, int buffer_s)
+{
+	text_type text;
+	text.size = 0;
+
+	while (1)
+	{
+		text = textReadStep(stream, text, buffer_s, terminator);
+		if (text.end == 1)
+		{
+			*(text.p + text.size - 1) = '\0';
+			if (stream != stdin)
+				fclose(stream);
+			return text;
+		}
+	}
+
+}
+
+int correctSize(text_type text)
+{
+	int sizeDelta = 0, i0 = 0;
+	for (int i = 0; (*(text.p + i) == ' ') || (*(text.p + i) == '\n'); i++)
+	{
+		sizeDelta--;
+		i0 = i;
+	}
+
+	for (int i = i0 + 1; i < text.size - 1; i++)
+	{
+		if (*(text.p + i) == ' ')
+		{
+			if ((*(text.p + i + 1) == ' ') ||
+				(*(text.p + i + 1) == '.') ||
+				(*(text.p + i + 1) == ',') ||
+				(*(text.p + i + 1) == '!') ||
+				(*(text.p + i + 1) == '?') ||
+				(*(text.p + i + 1) == '\0'))
+				sizeDelta--;
+		}
+		if ((*(text.p + i) == '.') ||
+			(*(text.p + i) == ',') ||
+			(*(text.p + i) == '!') ||
+			(*(text.p + i) == '?'))
+			if ((*(text.p + i + 1) != '.') &&
+				(*(text.p + i + 1) != ',') &&
+				(*(text.p + i + 1) != '!') &&
+				(*(text.p + i + 1) != '?') &&
+				(*(text.p + i + 1) != ' ') &&
+				(*(text.p + i + 1) != '\0'))
+				sizeDelta++;
+	}
+	return sizeDelta;
+}
+
+int symFirst(text_type text, text_type newtext)
+{
+	int i0 = 0;
+	for (int i = 0; i < text.size - 1; i++)
+		if ((*(text.p + i) != ' ') &&
+			(*(text.p + i) != '\n'))
+		{
+			i0 = i;
+			if ((*(text.p + i) >= 97) && (*(text.p + i) <= 122))
+				*newtext.p = *(text.p + i) - 32;
+			else
+				*newtext.p = *(text.p + i);
+			break;
+		}
+	return i0;
+}
+
+int symSpace(text_type text, text_type newtext, int i, int pos)
+{
+	if (*(text.p + i) == ' ')
+	{
+		if ((*(text.p + i + 1) != '.') &&
+			(*(text.p + i + 1) != ',') &&
+			(*(text.p + i + 1) != '!') &&
+			(*(text.p + i + 1) != '?') &&
+			(*(text.p + i + 1) != ' ') &&
+			(*(text.p + i + 1) != '\0'))
+		{
+			*(newtext.p + pos) = ' ';
+			return pos + 1;
+		}
+	}
+	return pos;
+}
+
+int symPunct(text_type text, text_type newtext, int i, int pos)
+{
+	if ((*(text.p + i) == ',') ||
+		(*(text.p + i) == '.') ||
+		(*(text.p + i) == '!') ||
+		(*(text.p + i) == '?'))
+	{
+		*(newtext.p + pos) = *(text.p + i);
+		if ((*(text.p + i + 1) != '.') &&
+			(*(text.p + i + 1) != ',') &&
+			(*(text.p + i + 1) != '!') &&
+			(*(text.p + i + 1) != '?') &&
+			(*(text.p + i + 1) != ' ') &&
+			(*(text.p + i + 1) != '\0'))
+		{
+			*(newtext.p + pos + 1) = ' ';
+			return pos + 2;
+		}
+		return pos + 1;
+	}
+	return pos;
+}
+
+int symLetter(text_type text, text_type newtext, int i, int pos)
+{
+	if ((*(text.p + i) != '.') &&
+		(*(text.p + i) != ',') &&
+		(*(text.p + i) != '!') &&
+		(*(text.p + i) != '?') &&
+		(*(text.p + i) != ' '))
+	{
+		*(newtext.p + pos) = *(text.p + i);
+		if ((*(newtext.p + pos) >= 65) && (*(newtext.p + pos) <= 90))
+			*(newtext.p + pos) += 32;
+		if ((*(newtext.p + pos) >= 97) && (*(newtext.p + pos) <= 122))
+		{
+			if (*(newtext.p + pos - 1) == ' ')
+				if ((*(newtext.p + pos - 2) == '.') ||
+					(*(newtext.p + pos - 2) == '!') ||
+					(*(newtext.p + pos - 2) == '?'))
+					*(newtext.p + pos) -= 32;
+			if (*(newtext.p + pos - 1) == '\n')
+				*(newtext.p + pos) -= 32;
+		}
+		return pos + 1;
+	}
+	return pos;
+}
+
+text_type textCorrect(text_type text)
+{
+	int i0, pos;
+	text_type newtext;
+	newtext.size = text.size + correctSize(text);
+	newtext.p = malloc(sizeof(char) * newtext.size);
+	if (newtext.p == NULL)
+	{
+		printf("textCorrect: No memory available! Can't correct text");
+		return text;
+	}
+	*(newtext.p + newtext.size - 1) = '\0';
+	i0 = symFirst(text, newtext);
+	pos = 1;
+	for (int i = i0 + 1; i < text.size - 1; i++)
+	{
+		pos = symSpace(text, newtext, i, pos);
+		pos = symPunct(text, newtext, i, pos);
+		pos = symLetter(text, newtext, i, pos);
+	}
+	return newtext;
+}
+
+FILE* streamInit()
+{
+	int stream_type;
+	FILE* stream = stdin;
+	char* fname[100];
+	char trash;
+	printf("Input stream type:\n");
+	printf("0 - stdin (default)\n");
+	printf("1 - file (input.txt)\n");
+	scanf_s("%d", &stream_type);
+	trash = getchar();
+	if (stream_type == 1)
+	{
+		if ((fopen_s(&stream, "input.txt", "r")) != 0)
+		{
+			printf("File open error\n");
+			return 0;
+		}
+	}
+	return stream;
+}
+
+void textPrint(char* text, FILE* stream)
+{
+	FILE* output;
+	if (stream == stdin)
+	{
+		printf("Corrected text:\n");
+		printf("%s\n", text);
+	}
+	else
+	{
+		if ((fopen_s(&output, "output.txt", "w")) != 0)
+		{
+			printf("File open error\n");
+			return 0;
+		}
+		printf("Text in output.txt\n");
+		fprintf(output, "%s\n", text);
+		fclose(output);
+	}
+}
+
+void main()
+{
+	text_type text, newtext;
+	char terminator = '#', trash, * terms[10];
+	int buffer_s;
+	FILE* stream = stdin, output;
+	stream = streamInit();
+	printf("Input terminator:\n");
+	fgets(terms, 10, stdin);
+	for (int i = 0; terms[i] != '\0'; i++)
+		if ((terms[i] != ' ') &&
+			(terms[i] != '\n'))
+		{
+			terminator = terms[i];
+			break;
+		}
+	printf("Input buffer size:\n");
+	scanf_s("%d", &buffer_s);
+	if (stream == stdin)
+		printf("Input text:\n");
+	trash = getchar();
+	text = getText(stream, terminator, buffer_s);
+	newtext = textCorrect(text);
+	textPrint(newtext.p, stream);
+	free(text.p);
+	free(newtext.p);
+
+}
