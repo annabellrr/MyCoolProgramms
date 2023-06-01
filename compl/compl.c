f20ad8fc8b9f4ab383d66857c9e98700c6fc663b
annabellrr <a.yusupova1@g.nsu.ru>
2023-06-01 22:56:07 +0700

    f

---
+++ compl/compl.c
@@ -0,0 +1,79 @@
+﻿//от Лысакова
+
+//решение задачки
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+typedef struct Complex
+{
+    double real;
+    double imag;
+} Complex;
+
+void complex_print(Complex numb)
+{
+    printf("%lf + %lfi\n", numb.real, numb.imag);
+}
+
+double complex_abs(Complex numb)
+{
+    return sqrt(numb.real * numb.real + numb.imag * numb.imag);
+}
+
+typedef struct array
+{
+    int size;
+    double* pMas;
+} array;
+
+double compare(double x, double y)
+{
+    return x > y;
+}
+
+double replace(double* x, double* y)
+{
+    double temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+array init_array(int size)
+{
+    array res;
+    res.size = size;
+    res.pMas = malloc(size * sizeof(double));
+    return res;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    FILE* fcomplex = fopen("chiselki.txt", "r");
+        if (fcomplex == NULL)
+            printf("Ошибка при открытии файла");
+
+    array chiselki = init_array(10);
+    for (int i = 0; i < chiselki.size; i++)
+        fscanf(&fcomplex, "%lf", chiselki.pMas + i);
+
+    for (int i = 0; i < 9; i++)
+        for (int j = 0; j < 9 - i; j++)
+        {
+            double a = complex_abs(&chiselki[i]);
+            double b = complex_abs(&chiselki[i + 1]);
+            if (compare(a, b))
+                replace(&a, &b);
+        }
+    printf("%lf", chiselki);
+
+
+    return 0;
+}
+
+
+
---
+++ test/test.c
@@ -0,0 +1,77 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+typedef struct Complex 
+{
+    double real;
+    double im;
+} Complex;
+
+Complex construct(double real, double im)
+{
+    Complex num;
+    num.real = real;
+    num.im = im;
+    return num;
+}
+
+double modOfComplex(Complex* number)
+{
+    return sqrt(number->real * number->real + number->im * number->im);
+}
+
+void readFromFile(const char* filename, Complex* numbers)
+{
+
+    FILE* file = fopen(filename, "r");
+    int i = 0;
+    double real, im;
+    while (fscanf(file, "%lf+%lfi", &real, &im) != EOF)
+    {
+        numbers[i] = construct(real, im);
+        i++;
+    }
+}
+
+void printComplex(Complex* number)
+{
+    printf("%.2f + i * %.2f\n", number->real, number->im);
+}
+
+void printComplexArray(Complex* numbers, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printComplex(&numbers[i]);
+    }
+}
+
+void sortByMod(Complex* nums, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (modOfComplex(&nums[i]) < modOfComplex(&nums[j]))
+            {
+                Complex temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    Complex array[3];
+    readFromFile("numbers.dat", array);
+    printComplexArray(array, 3);
+
+    printf("\n");
+
+    sortByMod(array, 3);
+    printComplexArray(array, 3);
+
+    return 0;
+}
