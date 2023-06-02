634dbb684be1a3059e933971a883377e86f947c5
annabellrr <a.yusupova1@g.nsu.ru>
2023-06-02 12:26:40 +0700

    game

--- game/game.cpp
+++ game/game.cpp
@@ -3,29 +3,31 @@
 #include <conio.h>
 #include <time.h>
 
+//в этой игрушке должна стоять англ раскладка
+//для передвижения используйте "WASD", для выхода - "e or q"
 
 int main()
 {
-	char mas[10][21];
+	char mas[12][26];
 	int i;
 	int x = 10, y = 5;
 	char key;
 	int ox, oy;
-	int ax = 5, ay = 3;
+	int ax = 6, ay = 4;
 	srand(time(NULL));
 
 	do
 	{
 		sprintf_s(mas[0], "####################");
-		for (i = 1; i < 9; i++)
+		for (i = 1; i < 11; i++)
 			sprintf_s(mas[i], "#                  #");
-		sprintf_s(mas[9], "####################");
+		sprintf_s(mas[11], "####################");
 
 		mas[y][x] = '@';
 		mas[ay][ax] = '*';
 
 		system("cls");
-		for (i = 0; i < 10; i++)
+		for (i = 0; i < 12; i++)
 			printf("%s\n", mas[i]);
 
 		key = _getch();
@@ -40,12 +42,13 @@ int main()
 		if (key == 'd') x++;
 		if (mas[y][x] == '#')
 		{
-			x = ox;
-			y = oy;
+			printf("\n\n\t\t\t\tGAME OVER, Mikhael Sergeevich! \the-he ^_^\n\n");
+			break;
 		}
+
 		if ((x == ax) && (y == ay))
 		{
-			ax = rand() * 1.0 / RAND_MAX * 18 + 1;
+			ax = rand() * 1.0 / RAND_MAX * 22 + 1;
 			ay = rand() * 1.0 / RAND_MAX * 8 + 1;
 		}
 
